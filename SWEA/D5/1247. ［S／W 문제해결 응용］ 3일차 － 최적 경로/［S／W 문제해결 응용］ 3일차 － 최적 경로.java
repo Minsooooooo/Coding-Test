1606a774a5d64553ec7fb4498944813a4bb52f3f
Minsooooooo <kinggodminsu@naver.com>
2026-08-16 17:52:27 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 1,917 ms, Memory: 28,068 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,116 +1,44 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.io.FileInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
+public class Solution {
     static int n;
-    static int[] company, home;
+    static int[][] company = new int[1][2], home = new int[1][2];
     static int[][] customer;
-
     static boolean[] visited;
-    static int[] seq;
 
+    static int[] order; //방문순서
     static int min;
-    
-	public static void dfs(int count){
-        if(count==n){
-            int distance = cal_distance();
-            min = Math.min(min, distance);
-            return;
-        }
 
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                seq[count] = i;
-                dfs(count+1);
-                visited[i] = false;
-            }
-        }
-
-    }
-
-
-
-
-    public static int cal_distance(){
-        int current_x = company[0];
-        int current_y = company[1];
-
-        int sum = 0;
-        for(int i=0; i<n; i++){
-            int num = seq[i];
-
-            sum += Math.abs(current_x-customer[num][0]) + Math.abs(current_y-customer[num][1]);
-            current_x = customer[num][0];
-            current_y = customer[num][1];
-
-        }
-        sum += Math.abs(current_x - home[0])+Math.abs(current_y - home[1]);
-
-        return sum;
-    }
-    
-    
-	public static void main(String args[]) throws Exception
-	{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    public static void main(String[] args) throws IOException {
+        
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-		int T;
-		T=Integer.parseInt(st.nextToken());
-		/*
-		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
+        
+        int test_case = Integer.parseInt(st.nextToken());
+        
+        for(int t=0; t<test_case; t++){
+            
             min = Integer.MAX_VALUE;
-			st = new StringTokenizer(br.readLine());
             
 
-            //고객 수 입력
-            n = Integer.parseInt(st.nextToken());
 
 
             st = new StringTokenizer(br.readLine());
 
-            company = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
-            home = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+            n = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+
+            company[0][0] = Integer.parseInt(st.nextToken());
+            company[0][1] = Integer.parseInt(st.nextToken());
+
+            home[0][0] = Integer.parseInt(st.nextToken());
+            home[0][1] = Integer.parseInt(st.nextToken());
+
             customer = new int[n][2];
 
             for(int i=0; i<n; i++){
@@ -121,12 +49,69 @@ class Solution
 
 
             visited = new boolean[n];
-            seq = new int[n];
+            order = new int[n];
 
             dfs(0);
 
-            System.out.println("#" +test_case+ " " + min);
+            System.out.println("#" + (t+1) + " " + min);
 
-		}
-	}
+        }
+
+
+
+    }
+
+    public static void dfs(int count){
+        if(count==n){
+            min = Math.min(min, cal_distance());
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                order[count] = i;
+                dfs(count+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static int cal_distance(){
+
+        int current_x = company[0][0];
+        int current_y = company[0][1];
+
+        int sum = 0;
+
+        for(int i=0; i<n; i++){
+            int num = order[i];
+            sum += Math.abs(current_x-customer[num][0]) + Math.abs(current_y - customer[num][1]);
+
+            current_x = customer[num][0];
+            current_y = customer[num][1];
+        }
+
+        sum += Math.abs(current_x-home[0][0]) + Math.abs(current_y - home[0][1]);
+
+        return sum;
+    }
 }
+
+
+/*
+N명의 고객을 방문하고 돌아가려함 (회사에서 출발 -> N명의 고객 방문 -> 집 도착)
+
+회사, 집, 고객 위치 = x,y
+
+두 위치 (x1, y1)와 (x2, y2) 사이의 거리는 |x1-x2| + |y1-y2|으로 계산된다.
+여기서 |x|는 x의 절대값을 의미하며 |3| = |-3| = 3이다. 회사의 좌표, 집의 좌표, 고객들의 좌표는 모두 다르다
+
+(회사에서 출발 -> N명의 고객 방문 -> 집 도착)에서 가장 잛은 경로를 찾으려함
+
+
+입력
+1. N (고객의 수)
+2. 회사좌표(x, y) 집 좌표(x,y) 고객수만큼 좌표 (x, y)
+
+ */
