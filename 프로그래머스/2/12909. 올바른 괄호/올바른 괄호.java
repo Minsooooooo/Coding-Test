300b52d29ab2f473470769c70ce57484ef429791
Minsooooooo <kinggodminsu@naver.com>
2026-09-07 11:20:14 +0900

    [level 2] Title: 올바른 괄호, Time: 13.33 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,29 +3,26 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        
+        Deque<String> deque = new ArrayDeque<>();
         int count = 0;
-        
-        int s_length = s.length();
-        for(int i=0; i<s_length; i++){
-            
-            if(s.charAt(i) == '('){
+        for(int i = 0; i < s.length(); i++){
+            deque.offerLast(Character.toString(s.charAt(i)));
+        }
+        while(!deque.isEmpty()){
+            String first = deque.pollFirst();
+            if(first.equals("(")){
                 count++;
             }else{
                 count--;
             }
-            
-            if(count<0){
-                return false;
+            if(count < 0){
+                answer = false;
+                break;
             }
         }
-        
-        if(count!=0){
-            return false;
+        if(count != 0){
+            answer = false;
         }
-
-        
-
         return answer;
     }
 }
